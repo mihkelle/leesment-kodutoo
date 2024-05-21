33c717cc761304f311981d249165f60a87f26e8b
Mihkelle <mihkel@leesment.ee>
2024-05-21 14:45:11 +0300

    First Commit of leesment-kodutoo web app

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+import requests
+
+app = Flask(__name__)
+
+server_ip = "<YOUR_VM_PUBLIC_IP>"
+server_port = 80
+
+
+@app.route("/")
+def index():
+    try:
+        response = requests.get(f"http://{server_ip}:{server_port}")
+        return response.text
+    except requests.exceptions.RequestException as e:
+        return str(e)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
