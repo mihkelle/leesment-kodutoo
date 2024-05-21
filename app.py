730431bc97306431faf76342fc019e8835d43467
Mihkelle <mihkel@leesment.ee>
2024-05-21 16:02:54 +0300

    more logs to find problem

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ server_ip = "4.211.158.21"
 server_port = 80
 
 
-# Tervisekontrolli lõpp-punkt
 @app.route("/health")
 def health():
     return "OK"
@@ -18,8 +17,10 @@ def index():
     try:
         response = requests.get(f"http://{server_ip}:{server_port}")
         response.raise_for_status()
+        app.logger.info(f"Successfully fetched data from VM: {response.text}")
         return response.text
     except requests.exceptions.RequestException as e:
+        app.logger.error(f"Error fetching data from VM: {str(e)}")
         return str(e), 500
 
 
